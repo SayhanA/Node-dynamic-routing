b2b080283e0150b8e31cc125aa86f5327788f4d1
Sayhan Ahmed <sayhan@ewnbd.com>
2024-12-14 06:34:27 +0600

    error page created

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const rootPath = require("./uttils/path");
+const get404 = require("./controllers/404");
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set('view engine', 'ejs');
+app.set("views", "views");
+
+app.use(get404);
+
+app.listen(PORT, () => console.log(`Server: http://localhost:${PORT}/`));
